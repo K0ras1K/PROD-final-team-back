8d80b13b524c0289205eadc4b59b8015e8c9e55e
Shaykhraziev Bulat <mrcookie209@gmail.com>
2024-03-31 13:55:45 +0300

    add member and remove member

--- src/main/kotlin/ru/droptableusers/sampleapi/database/schema/TeamsUsersTable.kt
+++ src/main/kotlin/ru/droptableusers/sampleapi/database/schema/TeamsUsersTable.kt
@@ -2,10 +2,10 @@ package ru.droptableusers.sampleapi.database.schema
 
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
 
-object TeamsUsersTable: IdTable<String>("teams_users") {
-    override val id: Column<EntityID<String>> = text("recordId").entityId()
-    val userId = reference("userId", UserTable.id)
+object TeamsUsersTable: IntIdTable("teams_users") {
+    val userId = reference("userId", UserTable.id).uniqueIndex()
     val teamId = reference("teamId", TeamTable.id)
 }
