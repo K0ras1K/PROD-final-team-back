0102a001ab5503f1c920e201e399d5ae771a0302
K0ras1K <romakalmykov@gmail.com>
2024-04-01 00:52:21 +0300

    feat: Add group validation

---
+++ src/main/kotlin/ru/droptableusers/sampleapi/controller/GroupAbstractController.kt
@@ -0,0 +1,17 @@
+package ru.droptableusers.sampleapi.controller
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import ru.droptableusers.sampleapi.data.enums.Group
+import ru.droptableusers.sampleapi.data.models.inout.output.ErrorResponse
+import ru.droptableusers.sampleapi.database.schema.GroupTable.group
+import ru.droptableusers.sampleapi.utils.GroupUtils
+
+abstract class GroupAbstractController(call: ApplicationCall) : AbstractController(call) {
+    suspend fun validateGroup(group: Group) {
+        if (!GroupUtils.hasGroup(login, group)) {
+            call.respond(HttpStatusCode.BadRequest, ErrorResponse("У вас недостаточно прав!"))
+        }
+    }
+}
