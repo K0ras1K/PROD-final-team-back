5c2635e628e2c61ca091a99e462394eb9ff09604
K0ras1K <romakalmykov@gmail.com>
2024-03-31 17:55:05 +0300

    fix: GroupTable

--- src/main/kotlin/ru/droptableusers/sampleapi/database/schema/GroupTable.kt
+++ src/main/kotlin/ru/droptableusers/sampleapi/database/schema/GroupTable.kt
@@ -4,6 +4,6 @@ import org.jetbrains.exposed.sql.Table
 import ru.droptableusers.sampleapi.data.enums.Group
 
 object GroupTable : Table("groups") {
-    val username = varchar("username", 50)
+    val username = reference("username", UserTable.username)
     val group = enumerationByName("group", 50, Group::class)
 }
