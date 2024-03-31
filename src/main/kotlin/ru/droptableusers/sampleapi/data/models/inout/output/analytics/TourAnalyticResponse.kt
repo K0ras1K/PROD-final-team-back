22e1d7b91a7fc569e05520395d2ae2b3166ebc6f
Кочергин Антон <sora.vwv@gmail.com>
2024-04-01 03:22:26 +0400

    Tour Analytics

---
+++ src/main/kotlin/ru/droptableusers/sampleapi/data/models/inout/output/analytics/TourAnalyticResponse.kt
@@ -0,0 +1,11 @@
+package ru.droptableusers.sampleapi.data.models.inout.output.analytics
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TourAnalyticResponse (
+    val name: String,
+    val averageMedian: Float,
+    val averageAlgebraic: Float,
+    val users: List<TourScoreAnalyticResponse>
+)
---
+++ src/main/kotlin/ru/droptableusers/sampleapi/data/models/inout/output/analytics/TourScoreAnalyticResponse.kt
@@ -0,0 +1,9 @@
+package ru.droptableusers.sampleapi.data.models.inout.output.analytics
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TourScoreAnalyticResponse (
+    val height: Float,
+    val count: Int
+)
