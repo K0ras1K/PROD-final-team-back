bcb9032173f2fbc3d553283abefcb1e8b1d551d3
Shaykhraziev Bulat <mrcookie209@gmail.com>
2024-04-02 02:12:19 +0300

    fix: onDelete cascade

--- src/main/kotlin/ru/droptableusers/sampleapi/database/schema/TelegramsTable.kt
+++ src/main/kotlin/ru/droptableusers/sampleapi/database/schema/TelegramsTable.kt
@@ -1,8 +1,9 @@
 package ru.droptableusers.sampleapi.database.schema
 
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
 object TelegramsTable : Table("telegrams") {
-    val userId = reference("userId", UserTable.id)
+    val userId = reference("userId", UserTable.id, onDelete = ReferenceOption.CASCADE)
     val telegramId = long("telegramId")
 }
