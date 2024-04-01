92674e1c49add06faae1c707a845532272ac3976
K0ras1K <romakalmykov@gmail.com>
2024-04-01 23:59:35 +0300

    feat: Add TelegramsTable

--- src/main/kotlin/ru/droptableusers/sampleapi/telegram/handler/TextHandler.kt
+++ src/main/kotlin/ru/droptableusers/sampleapi/telegram/handler/TextHandler.kt
@@ -1,6 +1,9 @@
 package ru.droptableusers.sampleapi.telegram.handler
 
 import com.pengrad.telegrambot.model.Message
+import ru.droptableusers.sampleapi.data.models.base.TelegramModel
+import ru.droptableusers.sampleapi.database.persistence.TelegramPersistence
+import ru.droptableusers.sampleapi.database.persistence.UserPersistence
 import ru.droptableusers.sampleapi.telegram.states.StateMachine
 import ru.droptableusers.sampleapi.telegram.templates.MainMessage
 
@@ -8,6 +11,21 @@ class TextHandler(val message: Message) {
     fun handle() {
         if (message.text() == "/start") {
             StateMachine.removeStatus(message.chat().id())
+            val userId =
+                UserPersistence().selectByTelegramId(
+                    "@${
+                        message.chat().username()
+                    }",
+                )!!.id
+            if (TelegramPersistence().selectByUserId(userId) == null
+            ) {
+                TelegramPersistence().insert(
+                    TelegramModel(
+                        userId = userId,
+                        telegramId = message.chat().id(),
+                    ),
+                )
+            }
             MainMessage(message.chat().id()).create()
         }
     }
