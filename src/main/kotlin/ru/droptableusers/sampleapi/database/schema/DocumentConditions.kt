aff64a19dd882c820ed5cd2b60def4735e72c7b9
Mihail Barinov <dmemsmdmemsm@gmail.com>
2024-04-01 03:01:04 +0300

    feat: Add documents tables

---
+++ src/main/kotlin/ru/droptableusers/sampleapi/database/schema/DocumentConditions.kt
@@ -0,0 +1,10 @@
+package ru.droptableusers.sampleapi.database.schema
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object DocumentConditions : IntIdTable("document_conditions") {
+    val documentId = reference("document_id", DocumentsTable.id)
+    val fieldName = varchar("field_name", 64)
+    val condition = varchar("condition", 16)
+    val value = varchar("value", 32)
+}
